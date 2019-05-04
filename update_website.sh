8bbd95b5d9585ad1c9d7c27fdceeaa03e84d9a12
paultpearson <paultpearson@gmail.com>
2019-05-04 10:30:26 -0400

    Initial commit

---
+++ update_website.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# A bash script for updating my github pages website
+
+# Add everything to your local git repository and push it up to your remote repository on GitHub:
+git add .
+git commit -m "Initial commit"
+git push -u origin master
+
+exit 0
